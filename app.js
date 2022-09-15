1077511a03c3b90dc2a5c11602cb33c3ecf39c93
zioscuro <lore.rang@gmail.com>
2022-09-16 00:13:12 +0200

    Add event listener on equals button to perform basic operations with the operate function

--- app.js
+++ app.js
@@ -15,6 +15,16 @@ function divide(a, b) {
   return a / b;
 }
 
+function selectOperator(operator) {
+  switch (operator) {
+    case 'add': return add;
+    case 'subtract': return subtract;
+    case 'multiply': return multiply;
+    case 'divide': return divide;
+    default: console.error('operator not supported!');
+  }
+}
+
 function operate(operation, a, b) {
   return operation(a, b);
 }
@@ -22,14 +32,39 @@ function operate(operation, a, b) {
 // DOM ELEMENTS
 const display = document.querySelector('#display');
 const digits = document.querySelectorAll('.digits button');
+const operators = document.querySelectorAll('.operators button');
+const equalsButton = document.querySelector('#equals');
 
 // CALCULATOR VARIABLES
 let displayValue = 0;
+let storedValue = 0;
+let currentOperator;
+let currentResult;
 
 // CALCULATOR BUTTONS EVENT LISTENERS
 digits.forEach((digit) => {
   digit.addEventListener('click', (e) => {
     displayValue = parseInt(display.textContent += e.target.textContent);
-    console.log(displayValue);
+    console.log('display: ' + displayValue);
   });
+});
+
+operators.forEach((operator) => {
+  operator.addEventListener('click', (e) => {
+    storedValue = displayValue;    
+    displayValue = 0;
+    display.textContent = '';
+    currentOperator = e.target.id;
+    console.log('stored: ' + storedValue);
+    console.log('operator: ' + currentOperator);
+  });
+});
+
+equalsButton.addEventListener('click', (e) => { 
+  currentResult = operate(selectOperator(currentOperator), storedValue, displayValue);
+
+  display.textContent = currentResult;
+  displayValue = 0;
+ 
+  console.log('current result: ' + currentResult);
 });
